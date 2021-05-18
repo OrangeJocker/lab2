009b5c61a387586726a56b88b63ee8b45c20960a
OrangeJocker <Jollydoy@gmail.com>
2021-05-19 06:44:21 +0700

    add do div

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -6,6 +6,10 @@ namespace Lab2
 {
     internal class Program
     {
+        private static float DoDiv(float a, float b)
+        {
+            return a / b;
+        }
         public static void Main(string[] args)
         {
         }
